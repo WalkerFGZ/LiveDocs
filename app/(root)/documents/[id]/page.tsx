3a88bfdda5af00f75736833884477583822f2acf
Andres Gallego <andresfelipegallegozuleta@gmail.com>
2025-01-31 10:53:17 -0500

    create header component

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,15 @@
 import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/ui/Header";
 import React from "react";
 
 const Documents = () => {
   return (
     <div>
+      <Header className="text-white">
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">This is a fake document title</p>
+        </div>
+      </Header>
       <Editor />
     </div>
   );
